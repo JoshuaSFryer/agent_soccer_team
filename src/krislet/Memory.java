4878531cd88d74d50a063ee373310ffb4a09d71d
David Bascelli <davidb996@protonmail.com>
2021-04-08 12:33:29 -0400

    Added found and close to knowledge base

--- src/krislet/Memory.java
+++ src/krislet/Memory.java
@@ -30,6 +30,14 @@ class Memory
     	percepts.clear();
     	if (m_info != null) {
         	percepts.add(Literal.parseLiteral("time(" + m_info.getTime() + ")"));
+        	for(int c = 0 ; c < m_info.m_objects.size() ; c ++) {
+        		ObjectInfo object = (ObjectInfo)m_info.m_objects.elementAt(c);
+        		// Jason freaks out if you try to add something to the knowledge base that has a space it in
+        		// Note to jason that we found the object
+        		percepts.add(Literal.parseLiteral("found(" + object.m_type.replace(" ", "_") + ")"));
+        		// note to jason that we are close to the object
+        		if(object.m_distance < 1) percepts.add(Literal.parseLiteral("close(" + object.m_type.replace(" ", "_") + ")"));
+    	    }	
     	}
     }
     
